701e2b05dde69434403ba6906e02211d1d561131
WhyDoesGodDoThis <101780103+WhyDoesGodDoThis@users.noreply.github.com>
2024-01-08 00:39:34 -0500

    updated test

--- src/test/java/entechlib/input/TestDriveInput.java
+++ src/test/java/entechlib/input/TestDriveInput.java
@@ -4,21 +4,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-public class TestDriveInput {
-    private static final DriveInput example = new DriveInput(0.75, 0.8, 0.3);
-
+class TestDriveInput {
     @Test
     void testGetRotationSpeed() {
+        DriveInput example = new DriveInput(0.75, 0.8, 0.3);
         assertEquals(0.3, example.getRotationSpeed());
     }
 
     @Test
     void testGetXSpeed() {
+        DriveInput example = new DriveInput(0.75, 0.8, 0.3);
         assertEquals(0.75, example.getXSpeed());
     }
 
     @Test
     void testGetYSpeed() {
+        DriveInput example = new DriveInput(0.75, 0.8, 0.3);
         assertEquals(0.8, example.getYSpeed());
     }
 }
