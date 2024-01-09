7178165c19b434095adb242213c01c01d0731d8a
WhyDoesGodDoThis <101780103+WhyDoesGodDoThis@users.noreply.github.com>
2024-01-09 14:49:41 -0500

    a test

---
+++ src/test/java/entechlib/swerve/exceptions/TextInvalidTypeException.java
@@ -0,0 +1,19 @@
+package entechlib.swerve.exceptions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TextInvalidTypeException {
+    @Test
+    void testGetMessageNull() {
+        Exception ex = new InvalidTypeException("Item", null);
+        assertEquals("Invalid Item type. Given: null", ex.getMessage());
+    }
+
+    @Test
+    void testGetMessageText() {
+        Exception ex = new InvalidTypeException("Item", "Wrong");
+        assertEquals("Invalid Item type. Given: Wrong", ex.getMessage());
+    }
+}
